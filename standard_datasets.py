a71bcbf1c8973654ed9999e4f7e5c80a1b0f1fb6
Algorithms-and-Poetry <algorithmsandpoetry@gmail.com>
2024-12-11 22:40:09 +0000

    changed versioning

--- standard_datasets.py
+++ standard_datasets.py
@@ -1,10 +1,10 @@
+import importlib
 import os
 from os.path import join
-import sys
-# sys.path.append("/datasets")
+
 datasets = {
     "quechua" : {
-        "1.0.0" : "quechua_1_0_0"
+        "1.0.0" : "quechua.v1_0_0"
     },
 }
 
@@ -30,17 +30,16 @@ def load_dataset(
     # check if dataset exists
     dataset_exists = name in datasets.keys()
     if not dataset_exists:
-        raise ValueError(f"There is no dataset with the name {name}")
+        raise ValueError(f"There is no dataset with the name '{name}'")
     
     # check if dataset exists
     version_exists = version in datasets[name].keys()
     if not version_exists:
-        raise ValueError(f"The dataset with the name {name} doesn't have a version {version}")
+        raise ValueError(f"The dataset with the name '{name}' doesn't have a version {version}")
 
     # make directories
-    # if not os.path.exists(dst):
     os.makedirs(dst, exist_ok=True)
 
     # import script and call its load_to function
-    script = __import__(datasets[name][version])
-    script.load_to(dst)
+    module = importlib.import_module(datasets[name][version])
+    module.load_to(dst)
